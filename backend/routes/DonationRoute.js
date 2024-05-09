3f784a6a0fe989ea88badfb716dfbadaddc2a498
Turkzy <165537971+Turkzy@users.noreply.github.com>
2024-05-10 03:14:50 +0800

    asdasd

--- backend/routes/DonationRoute.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { saveDonation } from "../controllers/DonationController.js";
-
-const router = express.Router();
-
-router.post('/Donation', saveDonation);
-
-export default router;
